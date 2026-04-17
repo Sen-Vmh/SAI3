8e01e2bfc46890aef64e1c3cb57011d242898618
Sen-Vmh <r0982669@student.thomasmore.be>
2026-04-17 12:16:33 +0200

    chore: add readme file with branch management rules

--- src/project/processing/pdf_extractor.py
+++ src/project/processing/pdf_extractor.py
@@ -10,10 +10,12 @@ def pdf_to_docling(source: Path) -> str:
     return convertor.convert(str(source)).document
     
 
-def pdf_export(source: Path, export_fn): # 
+def pdf_export(source: Path, export_fn):
     doc = pdf_to_docling(source)
 
     return export_fn(doc)
+
+
 
 for file_path in Path.iterdir(source_folder):
     if file_path.name == "data_files.md": 
