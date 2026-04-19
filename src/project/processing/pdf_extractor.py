5d209ffa523b0fe9a3784c1b0f54e133bee60885
Sen-Vmh <r0982669@student.thomasmore.be>
2026-04-19 14:42:54 +0200

    fix: add working cuda

--- src/project/processing/pdf_extractor.py
+++ src/project/processing/pdf_extractor.py
@@ -1,12 +1,26 @@
-from docling.document_converter import DocumentConverter
-import os
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling.backend.pypdfium2_backend import PyPdfiumDocumentBackend
+from docling.datamodel.pipeline_options import PdfPipelineOptions 
+from docling.datamodel.base_models import InputFormat   
+from docling.datamodel.accelerator_options import AcceleratorDevice, AcceleratorOptions
 from pathlib import Path
 import json
 
+pipeline_options = PdfFormatOption(
+            backend=PyPdfiumDocumentBackend,
+            pipeline_options=PdfPipelineOptions(
+                do_table_structure=True,
+                do_cell_matching=False, 
+                accelerator_options = AcceleratorOptions(device = AcceleratorDevice.CUDA)
+            )
+        )
+
+convertor = DocumentConverter(  
+    format_options={InputFormat.PDF: PdfFormatOption(pipeline_options=pipeline_options)}  
+)
+
 source_folder = Path("./data/raw/") # These file paths only work if your terminal is active in the root of the project
 output_folder = Path("./data/processed/")
-
-convertor = DocumentConverter()
 
 def pdf_export(source: Path, export_fn):
     result = convertor.convert(str(source))
@@ -23,7 +37,7 @@ def confirm_overwrite(path: Path) -> bool:
         return True
     
     while True:
-        choice = input(f"{path.name} already exists, do you want to overwrite this file? y/n")
+        choice = input(f"{path.name} already exists, do you want to overwrite this file? y/n ")
 
         if choice == "y":
             return True  
