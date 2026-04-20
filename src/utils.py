802463302d56a6e18ff9df1fdeec7eafa4b32225
Sen Vanmassenhove <145349956+Sen-Vmh@users.noreply.github.com>
2026-04-20 22:13:42 +0200

    Feat/docling to text (#10)
    * feat: create text generator file
    
    * chore: semantic rename of processing files
    
    * chore: add utils file
    
    * refactor: implement reusuble overwrite function
    
    * feat: add docling to file implementation

---
+++ src/utils.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+def confirm_overwrite(path: Path) -> bool:
+    if not path.exists():
+        return True
+    
+    while True:
+        choice = input(f"{path.name} already exists, do you want to overwrite this file? y/n ")
+
+        if choice == "y":
+            return True  
+        elif choice == "n":
+            print("Skipping file.")
+            return False
+        else:
+            print("Please enter 'y' or 'n'.")
+            
